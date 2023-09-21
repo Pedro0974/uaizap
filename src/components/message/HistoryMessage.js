40aad69d021d7dde41e6cc73830888f01cb1f855
Pedro <pedro0702lucas@gmail.com>
2023-09-20 22:35:03 -0300

    subindo styled components

--- src/components/message/HistoryMessage.js
+++ src/components/message/HistoryMessage.js
@@ -3,13 +3,12 @@ import Message from "./MessageComponent";
 
 const HistoryMessage = ({ messages, onRemoveMessage }) => {
   const [messageList, setMessageList] = useState([]);
+  const historyScrollRef = useRef(null);
 
   const sortedMessages = useMemo(
     () => [...messageList].sort((a, b) => new Date(b.time) - new Date(a.time)),
     [messageList]
   );
-
-  const historyScrollRef = useRef(null);
 
   useEffect(() => {
     setMessageList([...messages]);
@@ -17,25 +16,27 @@ const HistoryMessage = ({ messages, onRemoveMessage }) => {
 
   useEffect(() => {
     const historyScroll = historyScrollRef.current;
+
     if (historyScroll) {
-      historyScroll.scrollTop = 0;
+      const isScrolledToBottom = historyScroll.scrollHeight - historyScroll.scrollTop === historyScroll.clientHeight;
+      if (!isScrolledToBottom) {
+        historyScroll.scrollTop = historyScroll.scrollHeight;
+      }
     }
   }, [messageList]);
 
   const removeMessage = (messageToRemove) => {
     const updatedMessageList = messageList.filter((message) => message !== messageToRemove);
     setMessageList(updatedMessageList);
-  
     emitRemoveMessageEvent(messageToRemove);
   };
-  
 
   const emitRemoveMessageEvent = (messageToRemove) => {
     if (onRemoveMessage) {
       onRemoveMessage(messageToRemove);
     }
   };
-  
+
   return (
     <div className="history-scroll" ref={historyScrollRef}>
       <div className="container-history-message">
@@ -48,3 +49,4 @@ const HistoryMessage = ({ messages, onRemoveMessage }) => {
 };
 
 export default HistoryMessage;
+
--- src/components/message/MessageComponent.js
+++ src/components/message/MessageComponent.js
@@ -1,4 +1,48 @@
 import React, { useState, useEffect } from "react";
+import { styled } from "styled-components";
+
+const MessageContainer = styled.div`
+  background-color: #2A3942;
+  margin: 0.5rem 2rem;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  max-width: 40%;
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+
+  &.sent-scope {
+    background-color: #025C4C;
+  }
+
+  &.received {
+    color: #D9D9D9;
+  }
+`;
+
+const ScopeMessage = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+`;
+
+const ContextMessage = styled.p`
+  word-break: break-word;
+  ${({ isSentMessage }) =>
+    isSentMessage ? "padding-left: 1.4rem;" : "padding-right: 1.4rem;"}
+`;
+
+const TimeMessage = styled.div`
+  font-size: 0.75rem;
+  display: flex;
+  justify-content: ${({ isSentMessage }) =>
+    isSentMessage ? "flex-end" : "flex-start"};
+  align-items: center;
+`;
+
+const CheckIcon = styled.i`
+  padding: 0 0.1rem;
+`;
 
 const Message = ({ message, onRemove }) => {
   const [isSentMessage, setIsSentMessage] = useState(false);
@@ -23,10 +67,10 @@ const Message = ({ message, onRemove }) => {
   }, [message]);
 
   const handleDoubleClick = () => {
-    if(isSentMessage === true) {
+    if (isSentMessage === true) {
       setIsHintOpen(true);
     } else {
-      window.alert('You can only delete your messages')
+      window.alert("You can only delete your messages");
     }
   };
 
@@ -42,17 +86,31 @@ const Message = ({ message, onRemove }) => {
       className={`message-container ${isSentMessage ? "sent" : "received"}`}
       onDoubleClick={handleDoubleClick}
     >
-      <div className={`message ${isSentMessage ? "sent-scope" : "received"}`}>
-        <div className="scope-message">
-          <p className={`context-message ${isSentMessage ? "pr-14" : "pl-14"}`}>{message.context}</p>
-        </div>
-        <div className={`time-message  ${isSentMessage ? "justify-end" : "justify-start"}`}>
-          {messageTime} <i className="fa-solid fa-check-double px-1 "></i>
-        </div>
-      </div>
+      <MessageContainer
+        className={`message ${isSentMessage ? "sent-scope" : "received"}`}
+      >
+        <ScopeMessage className="scope-message">
+          <ContextMessage
+            className={`context-message ${isSentMessage ? "pr-14" : "pl-14"}`}
+            isSentMessage={isSentMessage}
+          >
+            {message.context}
+          </ContextMessage>
+        </ScopeMessage>
+        <TimeMessage
+          className={`time-message ${
+            isSentMessage ? "justify-end" : "justify-start"
+          }`}
+          isSentMessage={isSentMessage}
+        >
+          {messageTime} <CheckIcon className="fa-solid fa-check-double px-1" />
+        </TimeMessage>
+      </MessageContainer>
       {isHintOpen && (
         <div className="hint-sent">
-          <button onClick={handleDeleteClick}><i className="fa-solid fa-trash"></i></button>
+          <button onClick={handleDeleteClick}>
+            <i className="fa-solid fa-trash"></i>
+          </button>
         </div>
       )}
     </div>
